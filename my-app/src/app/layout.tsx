1bf159bb649c8fb515de665ad94c30ee5637dcd4
vaibhav <vaibhav0240gupta@gmail.com>
2024-06-10 11:50:46 +0530

    smart wallet integrated

--- my-app/src/app/layout.tsx
+++ my-app/src/app/layout.tsx
@@ -11,6 +11,10 @@ export const metadata = {
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <html lang="en">
+      <head>
+        <title>{metadata.title}</title>
+        <meta name="description" content={metadata.description} />
+      </head>
       <body className="bg-light-blue-500">
         <Navbar />
         <main className="flex flex-col items-center justify-center min-h-screen">
--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -1,11 +1,31 @@
-import React from 'react';
+// app/page.tsx
+"use client";
+import React, { useState } from 'react';
 import PacmanLogo from '../../components/PacmanLogo';
+import { BlueCreateWalletButton } from '../../components/CreateWalletButton';
 
 const Home: React.FC = () => {
+  const [account, setAccount] = useState<string | null>(null);
+
+  const handleSuccess = (address: string) => {
+    setAccount(address);
+  };
+
+  const handleError = (error: Error) => {
+    console.error('Error connecting to wallet:', error);
+  };
+
   return (
     <div className="flex flex-col items-center justify-center h-screen">
       <PacmanLogo />
       <h1 className="text-3xl font-bold mt-4">Click the PACMAN</h1>
+      {account ? (
+        <p className="mt-4">Connected account: {account}</p>
+      ) : (
+        <div className="mt-4">
+          <BlueCreateWalletButton handleSuccess={handleSuccess} handleError={handleError} />
+        </div>
+      )}
     </div>
   );
 };
