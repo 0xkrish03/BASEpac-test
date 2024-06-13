52a6f6d3d3bd4944517f057eb9acb6a0af7b8c60
vaibhav <vaibhav0240gupta@gmail.com>
2024-06-13 20:09:08 +0530

    frontend-restart

--- my-app/src/app/components/Footer.tsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="bg-blue-500 p-4 text-center text-white">
-      © 2024 PACMAN Site
-    </footer>
-  );
-}
--- my-app/src/app/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <div className="flex flex-col items-center justify-center h-screen">
-      <h1 className="text-3xl font-bold mt-4">Welcome to BASEpac</h1>
-    </div>
-  );
-}
